184b0c54ce880ba73134ba271b80c42d2e9c7a0b
dsx137 <1419696375@qq.com>
2023-03-15 01:43:01 +0800

    web window

--- src/mainWindow.cpp
+++ src/mainWindow.cpp
@@ -18,7 +18,17 @@ FWindow::UI* FWindow::getUI() {
 FWindow::UI::UI(QMainWindow* w) {
     w->resize(800, 600);
 
-    bt = new tButton(w);
-    bt->resize(100, 100);
-    QObject::connect(bt, &QPushButton::clicked, bt, &tButton::testSlotFunc);
+    // bt = new tButton(w);
+    // bt->resize(100, 100);
+    // QObject::connect(bt, &QPushButton::clicked, bt, &tButton::testSlotFunc);
+    view = new QWebEngineView(w);
+    view->load(QUrl("https://dsx137.github.io/DNote"));
+
+}
+
+void FWindow::resizeEvent(QResizeEvent* event)
+{
+    QMainWindow::resizeEvent(event); // 调用基类的方法
+    // ui->view->setGeometry(width() / 2, height() / 2, width() / 2, height() / 2); // 设置view的位置和大小
+    ui->view->setFixedSize(width(), height());
 }
--- src/mainWindow.h
+++ src/mainWindow.h
@@ -2,6 +2,7 @@
 #include <QtWidgets/QWidget>
 #include <QMainWindow>
 #include <QPushButton.h>
+#include <QWebEngineView>
 #include "abutton.h"
 
 
@@ -10,12 +11,14 @@ public:
     class UI {
     public:
         QWidget* mainwidget;
+        QWebEngineView* view;
         tButton* bt;
         UI(QMainWindow* w);
     };
     FWindow(QWidget* p = nullptr);
     ~FWindow();
     UI* getUI();
+    void resizeEvent(QResizeEvent* event);
 private:
     UI* ui;
 };
